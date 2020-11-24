af564bbbb28256c645d1582e5a46194b90fbe5b3
Voronko Maks <mars-sk8board@mail.ru>
2020-11-24 18:41:17 +0300

    Correct tests

--- test/lib/time.rb
+++ test/lib/time.rb
@@ -9,7 +9,11 @@ class TimeConvert
     result_array = []
     output = []
 
+    check_input(input)
+
     hour_conversion(input, minutes_array)
+
+    delete_inner_sector(minutes_array) if input.length > 1
 
     join_fragment(minutes_array)
 
@@ -21,6 +25,13 @@ class TimeConvert
   private
 
   attr_reader :sort_middle
+
+  def check_input(input)
+    mistakes = [nil, [], '', [''], ' ']
+    mistakes.each do |mistake|
+      abort 'Uncorrect input!' if input == mistake
+    end
+  end
 
   def hour_conversion(input, minutes_array)
     input.each do |x|
@@ -50,14 +61,14 @@ class TimeConvert
   def create_new_fragments(result_array, input)
     first_minute = 0
     last_minute = 1439
-    result_array << [@sort_middle[0][1], last_minute] if input.length <= 1
+    result_array << [@sort_middle[0][1], last_minute] if @sort_middle.length <= 1
     if_more_two_fragments(result_array, input, last_minute)
     result_array << [first_minute, @sort_middle[0][0]]
     result_array.sort!
   end
 
-  def if_more_two_fragments(result_array, input, last_minute)
-    return unless input.length >= 2
+  def if_more_two_fragments(result_array, _input, last_minute)
+    return unless @sort_middle.length >= 2
 
     result_array << [@sort_middle[0][1], @sort_middle[1][0]]
     result_array << [@sort_middle[1][1], last_minute]
@@ -69,6 +80,19 @@ class TimeConvert
     hours = '00' if hours.zero?
     rest = '00' if rest.zero?
     "#{hours}:#{rest}"
+  end
+
+  def delete_inner_sector(minutes_array)
+    (0..minutes_array.length).each do |i|
+      (0..1).each do |j|
+        if minutes_array[i][j + 1] >= minutes_array[i + 1][j + 1] && minutes_array[i][j] <= minutes_array[i + 1][j]
+          minutes_array.delete_at(1)
+          return minutes_array
+        else
+          return minutes_array
+        end
+      end
+    end
   end
 
   def convert(result_array, output)
--- test/spec/time_spec.rb
+++ test/spec/time_spec.rb
@@ -5,24 +5,48 @@ require_relative '../lib/time.rb'
 describe TimeConvert do
   let(:tc) { TimeConvert.new }
 
-  it 'gets three sections' do
+  it 'creates a TimeConvert class' do
+    expect(tc).to be_kind_of(TimeConvert)
+  end
+
+  it 'converts sections' do
+    expect(tc).to respond_to(:convert_fragment)
+  end
+
+  it 'converts five sections in three sections' do
     input = [['10:00', '10:20'], ['10:40', '11:00'], ['10:50', '12:00'], ['12:00', '13:00'], ['10:00', '10:20']]
     output = [['00:00', '10:00'], ['10:20', '10:40'], ['13:00', '23:59']]
 
     expect(tc.convert_fragment(input)).to eq(output)
   end
 
-  it 'get three sections, have two secttion' do
+  it 'converts two sections in three sections' do
     input =  [['10:00', '10:20'], ['10:40', '11:00']]
     output = [['00:00', '10:00'], ['10:20', '10:40'], ['11:00', '23:59']]
 
     expect(tc.convert_fragment(input)).to eq(output)
   end
 
-  it 'get two sections, have one secttion' do
+  it 'converts one section in two section' do
     input =  [['10:00', '10:20']]
     output = [['00:00', '10:00'], ['10:20', '23:59']]
 
     expect(tc.convert_fragment(input)).to eq(output)
+  end
+
+  it 'converts when time closed' do
+    input = [['10:00', '13:20'], ['11:00', '12:00']]
+    output = [['00:00', '10:00'], ['13:20', '23:59']]
+
+    expect(tc.convert_fragment(input)).to eq(output)
+  end
+
+  it 'enters invalid values' do
+    inputs = [nil, [], '', [''], ' ']
+    output = 'Uncorrect input!'
+
+    inputs.each do |input|
+      expect(tc.convert_fragment(input)).to eq(output)
+    end
   end
 end
