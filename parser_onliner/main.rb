0724426a6eaef83d7c7b21e85524d7f283095712
Voronko Maks <mars-sk8board@mail.ru>
2020-12-02 15:27:05 +0300

    add csv file

--- parser_onliner/main.rb
+++ parser_onliner/main.rb
@@ -3,24 +3,24 @@
 require 'open-uri'
 require 'byebug'
 require 'nokogiri'
+require 'csv'
 
 url = 'https://www.onliner.by'
 content = open(url).read
 
 document = Nokogiri::HTML(content)
-array = []
-document.css('a.b-teasers-2__teaser-i').each do |link|
-  url = link['href']
-  next if url.include? 'forum'
+csv_headers = %w[title image_link text]
+CSV.open('news.csv', 'wb') do |csv|
+  csv << csv_headers
+  document.css('a.b-teasers-2__teaser-i').each do |link|
+    url = link['href']
+    next if url.include? 'forum'
 
-  article = open(url).read
-  news_doc = Nokogiri::HTML(article)
-
-  news_hash = {
-    title: news_doc.css('div.news-header__title').text.strip,
-    image_link: news_doc.css('div.news-header__image')[0]['style'].sub('background-image: url(', '').sub(/\)\;/, ''),
-    text: news_doc.css('div.news-text p').text.slice(0..200)
-  }
-  array << news_hash
+    article = open(url).read
+    news_doc = Nokogiri::HTML(article)
+    title = news_doc.css('div.news-header__title').text.strip,
+            image_link = news_doc.css('div.news-header__image')[0]['style'].sub('background-image: url(', '').sub(/\)\;/, ''),
+            text = news_doc.css('div.news-text p').text.slice(0..200)
+    csv << [title, image_link, text]
+  end
 end
-puts array
